ed1408ab153eb53cafb43937d6bc0ffb6a573b6b
Tsahi kamar <tsahikamar@gmail.com>
2020-08-09 21:37:14 +0300

    SERVER FIX

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+//Orig app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/heroloWeather'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+//ORIG res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+
+res.sendFile(path.join(__dirname+'/dist/heroloWeather/index.html'));
+
 });
 
 // Start the app by listening on the default Heroku port
