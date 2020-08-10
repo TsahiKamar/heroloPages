9bab435d4ebbd8479d13f888b3758fc00bd8ec89
Tsahi kamar <tsahikamar@gmail.com>
2020-08-10 12:30:43 +0300

    pages commit

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+//USE BY Heroku
+
+//JSON PACKAGE
+//1. scripts
+//"heroku-postbuild": "ng build --prod"
+//2. At last section
+//"engines": {
+//    "node": "10.16.0",
+//    "npm": "6.14.7"
+//  }
+//3. type script ver - at dependencies ?
+//
+
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -5,13 +18,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-//Orig app.use(express.static(__dirname + '/dist/<name-of-app>'));
 app.use(express.static(__dirname + '/dist/heroloWeather'));
 
 app.get('/*', function(req,res) {
     
-//ORIG res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-
 res.sendFile(path.join(__dirname+'/dist/heroloWeather/index.html'));
 
 });
